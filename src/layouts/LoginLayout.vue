8e273c6ed1eda6d2849a4c45611733b062aae243
AbdElRhman-ElShorbagii <abdelrhmanelshorbagii@gmail.com>
2021-05-24 17:08:20 +0200

    adding login layout

--- src/layouts/LoginLayout.vue
+++ src/layouts/LoginLayout.vue
@@ -1,124 +1,97 @@
 <template>
-  <div class="centered-container">
-    <md-content class="md-elevation-3">
-      <div class="title">
-        <img src="https://vuematerial.io/assets/logo-color.png" />
-        <div class="md-title">Vue Material</div>
-        <div class="md-body-1">
-          Build beautiful apps with Material Design and Vue.js
-        </div>
-      </div>
-
-      <div class="form">
-        <md-field>
-          <label>E-mail</label>
-          <md-input v-model="login.email" autofocus></md-input>
-        </md-field>
-
-        <md-field md-has-password>
-          <label>Password</label>
-          <md-input v-model="login.password" type="password"></md-input>
-        </md-field>
-      </div>
-
-      <div class="actions md-layout md-alignment-center-space-between">
-        <a href="/resetpassword">Forget password</a>
-        <a href="/"
-          ><md-button class="md-raised md-primary" @click="auth"
-            >Log in</md-button
-          ></a
-        >
-      </div>
-
-      <div class="loading-overlay" v-if="loading">
-        <md-progress-spinner
-          md-mode="indeterminate"
-          :md-stroke="2"
-        ></md-progress-spinner>
-      </div>
-    </md-content>
-    <div class="background" />
-  </div>
+  <!-- <v-app id="login" class="secondary"> -->
+  <v-main>
+    <v-container fluid fill-height>
+      <v-layout align-center justify-center>
+        <v-flex xs12 sm6 md6 lg6>
+          <v-card class="elevation-2 pa-3">
+            <v-row>
+              <v-col>
+                <v-card-text>
+                  <div class="layout column align-center">
+                    <img src="@/assets/logo.png" alt="iSchool" width="180" height="180" />
+                    <h2 class="flex my-4 primary--text">Welcome Back!</h2>
+                  </div>
+                  <v-form @submit.prevent="login">
+                    <v-text-field
+                      append-icon="person"
+                      name="login"
+                      label="Email"
+                      v-model="email"
+                      :error="error"
+                      :rules="[rules.required]"
+                    />
+                    <v-text-field
+                      :type="hidePassword ? 'password' : 'text'"
+                      :append-icon="
+                        hidePassword ? 'visibility_off' : 'visibility'
+                      "
+                      name="password"
+                      label="Password"
+                      id="password"
+                      :rules="[rules.required]"
+                      v-model="password"
+                      :error="error"
+                      @click:append="hidePassword = !hidePassword"
+                    />
+                    <v-spacer></v-spacer>
+                    <v-btn
+                      block
+                      color="primary"
+                      type="submit"
+                      to="/dashboard"
+                      >Login</v-btn
+                    >
+                  </v-form>
+                </v-card-text>
+              </v-col>
+            </v-row>
+          </v-card>
+        </v-flex>
+      </v-layout>
+    </v-container>
+  </v-main>
+  <!-- </v-app> -->
 </template>
 
 <script>
 export default {
-  name: "LoginLayout",
   data() {
     return {
       loading: false,
-      login: {
-        email: "",
-        password: ""
-      }
+      email: "",
+      password: "",
+      hidePassword: true,
+      error: false,
+      rules: {
+        required: (value) => !!value || "Required.",
+      },
     };
   },
-  methods: {
-    auth() {
-      // your code to login user
-      // this is only for example of loading
-      this.loading = true;
-      setTimeout(() => {
-        this.loading = false;
-      }, 5000);
-    }
-  }
 };
 </script>
 
-<style lang="scss">
-.centered-container {
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  position: relative;
-  height: 100vh;
-  .title {
-    text-align: center;
-    margin-bottom: 30px;
-    img {
-      margin-bottom: 16px;
-      max-width: 80px;
-    }
-  }
-  .actions {
-    .md-button {
-      margin: 0;
-    }
-  }
-  .form {
-    margin-bottom: 60px;
-  }
-  .background {
-    background-color: rgb(220, 220, 220);
-    position: absolute;
-    height: 100%;
-    width: 100%;
-    top: 0;
-    bottom: 0;
-    right: 0;
-    left: 0;
-    z-index: 0;
-  }
-  .md-content {
-    z-index: 1;
-    padding: 40px;
-    width: 100%;
-    max-width: 400px;
+<style scoped lang="css">
+.div-wrapper {
     position: relative;
-  }
-  .loading-overlay {
-    z-index: 10;
-    top: 0;
-    left: 0;
-    right: 0;
-    position: absolute;
-    width: 100%;
-    height: 100%;
-    background: rgba(255, 255, 255, 0.9);
+    height: 130px;
+    width: 150px;
     display: flex;
-    align-items: center;
-    justify-content: center;
-  }
+    align-items: flex-end;
+}
+.div-wrapper img {
+  height: 130px;
+    width: 150px;
+    position: absolute;
+   
+}
+#login {
+  height: 50%;
+  width: 100%;
+  position: absolute;
+  top: 0;
+  left: 0;
+  content: "";
+  z-index: 0;
 }
 </style>
